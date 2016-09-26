f0add9e82333f323e3c716d9de6b2fd2f43d6b62
Scott Gauthreaux <me@scottgauthreaux.com>
2016-09-26 11:51:39 -0700

    Found one bug in revealAdjacent

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,26 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+
+/*
+TEST(FieldTest, getMineOutOfBounds) {
+    Field minefield;
+
+    minefield.placeMine(11,11);
+    //Don't know what type of exception this is throwing so can't get this to work
+    ASSERT_THROW(minefield.get(11,11));
+}
+*/
+
+TEST(FieldTest, revealAdjacent) {
+    Field minefield;
+
+    ASSERT_EQ(minefield.get(4,5), EMPTY_HIDDEN);
+    minefield.revealAdjacent(4,5);
+    ASSERT_EQ(minefield.get(4,5), EMPTY_SHOWN);
+    ASSERT_EQ(minefield.get(5,5), EMPTY_SHOWN);
+    ASSERT_EQ(minefield.get(3,5), EMPTY_SHOWN);
+    ASSERT_EQ(minefield.get(4,4), EMPTY_SHOWN);
+    ASSERT_EQ(minefield.get(4,6), EMPTY_SHOWN);
+}
--- buglog.md
+++ buglog.md
@@ -1,1 +1,10 @@
 # BUG LOG
+
+## Found issue in `revealAdjacent`
+
+Reveal adjacent should update the status of the location to `EMPTY_SHOWN` when the location contains `EMPTY_HIDDEN`.
+Test 2 concludes that the statis of the location is not updated to `EMPTY_SHOWN` despite the location containing `EMPTY_HIDDEN` prior to calling `revealAdjacent`
+I suspect the following line is breaking the code causing the function to immediately return when the location is in bounds:
+```cpp
+    if( inBounds(col,row) || _map[col][row] == MINE_HIDDEN || _map[col][row] == MINE_SHOWN )
+```
